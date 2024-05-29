a4582a14e5e64a288a274290bbfc27e435e251dc
giraffe <2195498051@qq.com>
2024-05-29 22:54:40 +0800

    preview

---
+++ app/src/main/java/com/example/myapp1/restart/previewC.kt
@@ -0,0 +1,16 @@
+package com.example.myapp1.restart
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showSystemUi = true, group = "t1", showBackground = true,name="pre1")
+@Composable
+fun previewV(){
+    Text("hello world")
+}
+@Preview(showSystemUi = true, group = "t2", showBackground = true,name="pre1")
+@Composable
+fun previewV2(){
+    Text("hello world2")
+}
