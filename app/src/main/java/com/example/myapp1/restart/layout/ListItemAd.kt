6d89fb305f9e3d27bcc7684665f07ae8c0ec105e
小丑在唱独角戏 <10097251+xczcdjx@user.noreply.gitee.com>
2024-05-30 13:13:16 +0800

    listItem调整

--- app/src/main/java/com/example/myapp1/restart/layout/ListItemAd.kt
+++ app/src/main/java/com/example/myapp1/restart/layout/ListItemAd.kt
@@ -1,50 +1,66 @@
 package com.example.myapp1.restart.layout
 
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapp1.utils.L
 
-class News(var t:String,var f:Boolean){
+class News(var t: String, var f: Boolean) {
     override fun toString(): String {
         return "Newsttttt(t='$t', f=$f)"
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun ListItemAd(){
-    val ns= List<News>(10){News("day23_$it",false)}
-    val nsB= List(10){false}
+fun ListItemAd() {
+    val ns = List<News>(10) { News("day23_$it", false) }
+    val nsB = List(10) { false }
     var list by remember {
         mutableStateOf(ns)
     }
     var list2 by remember {
         mutableStateOf(nsB)
     }
-    L.log("list111=$list")
-    L.log("list222=$list2")
-    list2.forEachIndexed{i,it->
+    list2.forEachIndexed { rowIndex, item ->
         ListItem(
+            leadingContent = {
+                Icon(
+                    imageVector = Icons.Default.AccountBox, contentDescription = null, modifier =
+                    Modifier.size(50.dp)
+                )
+            },
             headlineContent = {
-                Text(text = list[i].t)
+                // 主标题
+                Text(text = list[rowIndex].t)
+            },
+
+            supportingContent = {
+                Text(text = "详情")
+            },
+            overlineContent = {
+                Text(text = "副标题")
             },
             trailingContent = {
-                Checkbox(checked = list2[i], onCheckedChange = {
-                   /* list=list.mapIndexed{
-                        ni,li->
-                        if (i==ni) li.f=!li.f
-                        else li.f=li.f
-                        li
-                    }*/
-                    list2=list2.mapIndexed{
-                            ni,li->
-                        if (i==ni) !it
+                Checkbox(checked = item, onCheckedChange = {
+                    /* list=list.mapIndexed{
+                         ni,li->
+                         if (i==ni) li.f=!li.f
+                         else li.f=li.f
+                         li
+                     }*/
+                    list2 = list2.mapIndexed { newIndex, li ->
+                        if (rowIndex == newIndex) !li
                         else li
                     }
-                    L.log("list22=$list")
-                    L.log("list2222=$list2")
                 })
             }
         )
